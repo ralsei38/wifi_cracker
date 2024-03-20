27da7c2e71a77db50ebb024e7c25333ae52768c7
Théo Fontana <fontana.theo.me@gmail.com>
2024-03-20 23:46:15 +0100

    ongoing headaches

--- association.py
+++ association.py
@@ -1,24 +1,44 @@
 from scapy.all import Dot11, Dot11AssoReq, Dot11Beacon, Dot11Elt, Dot11EltRates, Dot11EltRSN, AKMSuite, RSNCipherSuite, RadioTap, srp1
 import pdb
 # ASSOCIATION PHASE-------
+def get_elt(pkt, ID) -> Dot11Elt:
+    """Returns the information element for a given ID"""
+    layer = pkt.getlayer(Dot11Elt)
+    while layer is not None and layer.ID != ID:
+        layer = layer.getlayer(Dot11Elt, 2)
+    return layer
+
 def association(own_MAC, AP_beacon, iface):
     AP_mac = AP_beacon.addr2
     cap = AP_beacon.cap
-    rates = AP_beacon.rates
+    rates = AP_beacon.getlayer(Dot11EltRates).rates
     ssid = AP_beacon.info.decode()
     print("ASSOCIATION PHASE !!!")
-    dot11_header = Dot11(subtype=0, type=0, proto=0, addr1=AP_mac, addr2=own_MAC, addr3=AP_mac)
+    dot11_header = Dot11(subtype=0, type=0, addr1=AP_mac, addr2=own_MAC, addr3=AP_mac)
     dott11_element_assoreq = Dot11AssoReq(cap=cap, listen_interval=20)
     dott11_element_ssid = Dot11Elt(ID=0, info=ssid, len=len(ssid))
+    dott11_element_ssid = Dot11EltRates(ID=1, rates=rates, len=len(rates))
+    es_rates = get_elt(AP_beacon, 50)
+    if es_rates is not None:
+        es_rates = Dot11Elt(
+            ID=50,
+            info=es_rates.info,
+            len=len(es_rates.info)
+        )
+    pkt = RadioTap() / dot11_header / dott11_element_assoreq / dott11_element_ssid
+    if es_rates is not None:
+        pkt /= es_rates
+    
     dott11_element_RSN = Dot11EltRSN(    
         ID=48,    
         len=20,    
-        version=1,    
+        version=1,
         group_cipher_suite=RSNCipherSuite(cipher='CCMP-128'),    
         nb_pairwise_cipher_suites=1,    
         pairwise_cipher_suites=RSNCipherSuite(cipher='CCMP-128'),    
         nb_akm_suites=1,    
         akm_suites=AKMSuite(suite='PSK'))
-    pkt = RadioTap()/dot11_header/dott11_element_assoreq/dott11_element_ssid/dott11_element_RSN
+    pkt /= dott11_element_RSN
     result = srp1(pkt, iface=iface)
+    pdb.set_trace()
 # ASSOCIATION PHASE-------
--- authentication.py
+++ authentication.py
@@ -4,10 +4,10 @@ from scapy.all import Dot11, Dot11Auth, RadioTap, srp1
 # I have to first authenticate and associate. After successful association, AP finally sends an EAP Request Identity packet.
 def authentication(own_MAC: str, AP_beacon: str, iface: str):
     mac_header = Dot11(subtype=11, type=0, proto=0, addr1=AP_beacon.addr2, addr2=own_MAC, addr3=AP_beacon.addr2)
-    body_frame = Dot11Auth(algo=0, seqnum=1) #Open System authentication, then use EAP to identify later on...
+    body_frame = Dot11Auth(algo=0, seqnum=1, status='success') #Open System authentication, then use EAP to identify later on...
     pkt = RadioTap()/mac_header/body_frame
     print("AUTHENTICATION PHASE !!!")
-    result = srp1(pkt, iface=iface)
+    result = srp1(pkt, iface=iface, inter=2)
     # result.show()
     if result.haslayer(Dot11Auth) and result.getlayer(Dot11Auth).status == 0:
         print("Open system authentication: Success")
